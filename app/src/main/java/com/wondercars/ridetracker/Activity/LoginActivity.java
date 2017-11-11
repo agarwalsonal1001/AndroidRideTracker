8dccb6066c60ef534eb5ff033b8d6f0a5510ca33
umer <umer.sayyed1989@gmail.com>
2017-11-11 13:48:02 +0530

    Added Login screen By Umer Sayyed

--- app/src/main/java/com/wondercars/ridetracker/Activity/LoginActivity.java
+++ app/src/main/java/com/wondercars/ridetracker/Activity/LoginActivity.java
@@ -1,8 +1,10 @@
 package com.wondercars.ridetracker.Activity;
 
 import android.app.ProgressDialog;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import com.wondercars.ridetracker.BaseClasses.BaseActivity;
 import com.wondercars.ridetracker.R;
@@ -10,31 +12,44 @@ import com.wondercars.ridetracker.Retrofit.DTOs.LoginServiceDTOs.LoginRequestObj
 import com.wondercars.ridetracker.Retrofit.DTOs.LoginServiceDTOs.LoginResponseObj;
 import com.wondercars.ridetracker.Utils.APIConstants;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 import umer.accl.interfaces.RetrofitInterface;
 import umer.accl.retrofit.RetrofitParamsDTO;
 import umer.accl.utils.Constants;
 
+import static com.wondercars.ridetracker.Utils.AppConstants.PLEASE_ENTER_PASSWORD;
+import static com.wondercars.ridetracker.Utils.AppConstants.PLEASE_ENTER_USERNAME;
+
 public class LoginActivity extends BaseActivity {
 
     private static final int USER_LOGIN_API = 1;
+    @BindView(R.id.edt_enter_username)
+    EditText edtEnterUsername;
+    @BindView(R.id.edt_enter_password)
+    EditText edtEnterPassword;
+    @BindView(R.id.tv_login)
+    TextView tvLogin;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-        callUserLoginApi();
+        ButterKnife.bind(this);
+      //  callUserLoginApi();
 
     }
 
     private LoginRequestObj getLoginApiRequestObject() {
         LoginRequestObj loginRequestObj = new LoginRequestObj();
-        loginRequestObj.setUsername("admin");
-        loginRequestObj.setPassword("admin123");
+        loginRequestObj.setUsername(edtEnterUsername.getText().toString());
+        loginRequestObj.setPassword(edtEnterPassword.getText().toString());
         return loginRequestObj;
     }
 
     private void callUserLoginApi() {
-        RetrofitParamsDTO retrofitParamsDTO = new RetrofitParamsDTO.RetrofitBuilder(LoginActivity.this, APIConstants.baseurl,getLoginApiRequestObject(), LoginResponseObj.class, Constants.RetrofitMethodConstants.LOGIN_API, USER_LOGIN_API, Constants.ApiMethods.POST_METHOD,retrofitInterface)
+        RetrofitParamsDTO retrofitParamsDTO = new RetrofitParamsDTO.RetrofitBuilder(LoginActivity.this, APIConstants.baseurl, getLoginApiRequestObject(), LoginResponseObj.class, Constants.RetrofitMethodConstants.LOGIN_API, USER_LOGIN_API, Constants.ApiMethods.POST_METHOD, retrofitInterface)
                 .setProgressDialog(new ProgressDialog(this))
                 .setShowDialog(true)
                 .build();
@@ -52,8 +67,27 @@ public class LoginActivity extends BaseActivity {
 
         @Override
         public void onError(int serviceId) {
-            showLongToast(Constants.ToastMessages.SOMETHING_WENT_WRONG);
+            //showLongToast(Constants.ToastMessages.SOMETHING_WENT_WRONG);
         }
     };
 
+    private boolean validateFields() {
+        if (TextUtils.isEmpty(edtEnterUsername.getText().toString())) {
+            showShortToast(PLEASE_ENTER_USERNAME);
+            return false;
+        }
+        if (TextUtils.isEmpty(edtEnterPassword.getText().toString())) {
+            showShortToast(PLEASE_ENTER_PASSWORD);
+            return false;
+        }
+
+        return true;
+    }
+
+    @OnClick(R.id.tv_login)
+    public void onClick() {
+        if (validateFields()) {
+            callUserLoginApi();
+        }
+    }
 }
