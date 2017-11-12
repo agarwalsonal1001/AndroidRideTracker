859f0e0dbf43a611376cfe432b0cfe5aa6d7e01c
umer <umer.sayyed1989@gmail.com>
2017-11-12 17:12:46 +0530

    added dime and all drawable files By Umer Sayyed

---
+++ app/src/main/java/com/wondercars/ridetracker/Utils/AppConstants.java
@@ -0,0 +1,12 @@
+package com.wondercars.ridetracker.Utils;
+
+/**
+ * Created by umer on 11/11/17.
+ */
+
+public interface AppConstants {
+
+    String PLEASE_ENTER_USERNAME = "Please enter username";
+    String PLEASE_ENTER_PASSWORD = "Please enter password";
+
+}
